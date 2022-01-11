9c240726e84fb7ba750f0cb87aad23c8cabb2d73
Austin Moore <austinchristianmoore@gmail.com>
2022-01-11 18:04:36 -0500

    Feature: Pass It2 Event tests

--- lib/event.rb
+++ lib/event.rb
@@ -8,4 +8,26 @@ attr_reader :name,
     @crafts = [craft]
     @attendees = [person]
   end
+
+  def attendee_names
+    @attendees.flatten.map do |attendee| #why do i have to flatten this
+      attendee.name
+    end
+  end
+
+  def craft_with_most_supplies #select would probably be better
+    craft_pair = @crafts.flatten.map do |craft|
+      [craft.supplies_required.length, craft.name]
+    end
+    craft_pair.to_h.max.last
+  end
+
+  def supply_list
+    list = @crafts.flatten.map do |craft|
+      craft.supplies_required.keys
+    end
+    return list.flatten.uniq.map do |supply|
+      supply.to_s
+    end
+  end
 end
