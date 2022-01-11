2cfe87add06bda3e653227bd60f278205f4c07b0
Austin Moore <austinchristianmoore@gmail.com>
2022-01-11 16:22:16 -0500

    Clear work, reattempt

--- lib/craft.rb
+++
@@ -1,9 +0,0 @@
-class Craft
-  attr_reader :name,
-              :supplies_required
-
-  def initialize(name, supplies_required)
-    @name              = name
-    @supplies_required = supplies_required
-  end
-end
