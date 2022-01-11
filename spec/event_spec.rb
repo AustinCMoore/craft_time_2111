9e80e784e51afd82496640ada03ee082897bc6a4
Austin Moore <austinchristianmoore@gmail.com>
2022-01-11 17:07:29 -0500

    Feature: Build Iteration 2 tests

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -28,3 +28,44 @@ RSpec.describe Event do
     expect(@event.attendees).to eq([@person])
   end
 end
+
+RSpec.describe "Iteration 2" do
+  before (:each) do
+    @hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    @toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+    @sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+    @knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    @event = Event.new("Carla's Craft Connection", [@sewing, @knitting], [@hector, @toni])
+  end
+
+  it "has new attendees" do
+    expect(@event.attendee_names).to eq(["Hector", "Toni"])
+  end
+
+  it "returns craft with most supplies" do
+    expect(@event.craft_with_most_supplies).to be_instance_of Array
+    expect(@event.craft_with_most_supplies).to eq("sewing")
+  end
+
+  it "returns supply list" do
+    expect(@event.supply_list).to be_instance_of Array
+    expect(@event.supply_list).to eq(["fabric", "scissors", "thread", "sewing_needles", "yarn", "knitting_needles"])
+  end
+
+  it "builds if a person has the supplies for a craft" do
+    @hector.can_build?(@sewing)
+    expect(@hector.can_build?(@sewing)).to be_instance_of Boolean
+    expect(@hector.can_build?(@sewing)).to eq(false)
+
+    @hector.add_supply('fabric', 7)
+    @hector.add_supply('thread', 1)
+    @hector.can_build?(@sewing)
+    expect(@hector.can_build?(@sewing)).to eq(false)
+
+    @hector.add_supply('scissors', 1)
+    @hector.add_supply('sewing_needles', 1)
+    @hector.can_build?(@sewing)
+    expect(@hector.can_build?(@sewing)).to eq(true)
+  end
+
+end
